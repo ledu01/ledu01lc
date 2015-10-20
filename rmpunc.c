0f530ff991aa91238edd25500d4a482e9be76f30
Duc Le <ledu01@8689.local>
2015-10-20 13:33:08 -0500

    Initial Commit

---
+++ rmpunc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "ispunc.h"
+
+int main(int argc, char* argv[]) {
+/*	char c;
+	c = getchar();
+	while (c != EOF) {
+		if (!ispunc(c))
+			printf("%c",c);
+		c=getchar();
+	}
+*/
+	char c;
+	while ((c=getchar()) != EOF) {
+		if (!ispunc(c))	
+			printf("%c",c);
+	
+	}
+	return 0;
+}
